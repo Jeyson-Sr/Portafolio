d312081e1e8f59d1e9c51d2c628a826e76424c7e
Jhosep <jhosep.remigio2005@gmail.con>
2025-06-25 10:07:08 -0500

    Fix: imagenes

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -1,10 +1,15 @@
 import { Proyecto } from "../components/Proyecto";
+import ToDoImage from "../assets/To-Do-Lists.png"; // ruta correcta según dónde esté
+import FinazasPersonales from "../assets/Finazas-Personales.png"; // ruta correcta según dónde esté
+
+
+
 
 function Proyectos() {
   return (
     <div className="flex flex-col gap-10 ">
       <Proyecto
-        imagen={"/assets/To-Do-Lists.png"}
+        imagen={ToDoImage}
         titulo="To-Do Lists"
         descripcion="App web para crear, marcar, eliminar y filtrar tareas con una interfaz minimalista e intuitiva. Ideal para organizar tu día de manera rápida y sin complicaciones."
         tecnologias={[
@@ -16,7 +21,7 @@ function Proyectos() {
         enlace="https://to-do-list-self-kappa-76.vercel.app/"
       />
       <Proyecto
-        imagen={"/assets/Finazas-Personales.png"}
+        imagen={FinazasPersonales}
         titulo="Finanzas Personales"
         descripcion="Aplicación web para gestionar ingresos y gastos, visualizar balance en gráficos, recibir alertas por exceso de gastos y organizar transacciones. Interfaz clara y adaptable a móviles."
         tecnologias={[
