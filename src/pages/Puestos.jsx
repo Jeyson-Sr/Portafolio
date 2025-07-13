81f655cc96594a95c16dce17949dc159aa0e9f38
Jeyson-Sr <jhosep.remigio2005@gmail.com>
2025-07-13 18:01:06 -0500

    Add: Exp in EvoxLab

--- src/pages/Puestos.jsx
+++ src/pages/Puestos.jsx
@@ -3,16 +3,20 @@ import { Puesto } from "../components/Puesto";
 function Puestos() {
   return (
     <div className="flex flex-col gap-10">
-      <Puesto
-  año="2024/02 — 2024/06"
-  cargo="Desarrollador Full Stack"
-  empresa='Academia de Karate "Jhire"'
-  descripcion="Desarrollo de una plataforma para gestionar inscripciones, clases y pagos de alumnos. 
-               • Creación de interfaces limpias y fáciles de usar, optimizando la experiencia del usuario.
-               • Implementación de autenticación básica y estructuración de rutas REST."
-  tecnologias={["React", "Node.js", "MySQL"]}
-/>
 
+<Puesto
+  año="2025/02 — 2025/06"
+  cargo="Desarrollador Web"
+  empresa="Evoxlab"
+  descripcion="Diseñé y desarrollé plataformas web:
+              • Construí el backend con PHP sobre CodeIgniter4, definiendo rutas, controladores y modelos para gestionar usuarios, clases, pagos e
+                inscripciones.
+              • Implementé el frontend con HTML, TailwindCSS y JavaScript, creando interfaces responsivas e interactivas.
+              • Integré pasarelas de pago como Stripe, configurando webhooks y asegurando transacciones seguras y fluidas.
+              • Desarrollé sistemas de autenticación y validación de datos, y optimicé consultas a la base de datos para garantizar rendimiento y seguridad.
+              • Colaboré con el equipo de diseño para mejorar la experiencia de usuario, logrando una navegación fluida y tiempos de carga reducidos"
+  tecnologias={["HTML", "PHP", "JavaScript", "Tailwind", "CodeIgniter4"]}
+/>
 <Puesto
   año="2024/08 — 2024/11"
   cargo="Desarrollador Frontend Freelance"
@@ -22,8 +26,18 @@ function Puestos() {
   tecnologias={["HTML", "CSS", "JavaScript", "Tailwind", "React"]}
 />
 
+      <Puesto
+  año="2024/02 — 2024/07"
+  cargo="Desarrollador Full Stack"
+  empresa='Academia de Karate "Jhire"'
+  descripcion="Desarrollo de una plataforma para gestionar inscripciones, clases y pagos de alumnos. 
+               • Creación de interfaces limpias y fáciles de usar, optimizando la experiencia del usuario.
+               • Implementación de autenticación básica y estructuración de rutas REST."
+  tecnologias={["React", "Node.js", "MySQL"]}
+/>
+
 <Puesto
-  año="2023/02 — 2023/06"
+  año="2023/02 — 2023/07"
   cargo="Desarrollador de Software (Prácticas Preprofesionales)"
   empresa="IE 20827 Mercedes Indacochea Lozano"
   descripcion="Desarrollé un sistema de gestión de almacenamiento de materiales para una institución educativa. 
@@ -32,7 +46,7 @@ function Puestos() {
 />
 
 <Puesto
-  año="2023/07 — 2023/09"
+  año="2023/07 — 2023/11"
   cargo="Desarrollador Frontend"
   empresa="Inova"
   descripcion="Desarrollé una interfaz en JavaScript para gestionar preguntas de mantenimiento para distintas máquinas. 
